1076f4500f1315a3e92fdb77ab2cbb65d1a3bc2a
gjwjddbs <apexgenuine@gmail.com>
2025-03-07 23:06:26 +0900

    Initial commit: Flask 프로젝트 초기 설정

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+#ORM 설정 파일
+
+BASE_DIR = os.path.dirname(__file__)
+
+#SQLALCHEMY_DATABASE_URI는 데이터베이스 접속 주소
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+
+#SQLALCHEMY 이벤트 처리 옵션
+SQLALCHEMY_TRACK_MODIFICATIONS = False
