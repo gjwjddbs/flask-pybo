95f70c79bea835bea5d12a3b7e27bf648ce55440
gjwjddbs <apexgenuine@gmail.com>
2025-03-15 20:45:29 +0900

    [200~feat: 질문 및 답변 등록 기능 완성

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request
+from flask import Blueprint, url_for, request, render_template
 from werkzeug.utils import redirect
 
-from pybo import db 
+from ..forms import AnswerForm
 
-from pybo.models import Question, Answer
+from .. import db 
+
+from ..models import Question, Answer
 
 #answer_views.py 파일이 answer라는 이름의 블루프린트 파일임을 나타냄
 bp = Blueprint('answer',__name__,url_prefix='/answer')
@@ -14,11 +16,14 @@ bp = Blueprint('answer',__name__,url_prefix='/answer')
 @bp.route('/create/<int:question_id>',methods=('POST',))
 #답변 저장 템플릿에 있는 form 태그의 action 속성에 해당하는 URL을 나타낸다
 def create(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
     #name 속성이 content인 form 태그의 데이터를 request.form['content']로 가져온다
-    content=request.form['content']
-    answer = Answer(content=content,create_date=datetime.now())
-    #질문과 답변은 1:N 관계이므로 질문에 답변을 추가하기 위해 append() 메서드를 사용한다
-    question.answer_set.append(answer)
-    db.session.commmit()
-    return redirect(url_for('question.detail',question_id=question_id))
+    #답변 저장 버튼을 누르면 POST 방식으로 전송되었으면 form.validate_on_submit()이 참이 되어 답변을 저장한다
+    if form.validate_on_submit():
+        content=request.form['content']
+        answer = Answer(content=content, create_date=datetime.now())
+        question.answer_set.append(answer)
+        db.session.commit()
+        return redirect(url_for('question.detail',question_id=question_id))
+    return render_template('question/question_detail.html',question=question,form=form)
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -4,7 +4,7 @@ from flask import Blueprint , render_template, request, redirect, url_for
 
 from pybo.models import Question
 
-from ..forms import QuestionForm 
+from ..forms import QuestionForm, AnswerForm 
 
 from werkzeug.utils import redirect
 
@@ -19,8 +19,9 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form=AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html',question=question)
+    return render_template('question/question_detail.html',question=question,form=form)
 
 @bp.route('/create/', methods=('GET','POST'))
 def create():
@@ -29,5 +30,5 @@ def create():
         question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
         db.session.add(question)
         db.session.commit()
-        return redirect(url_for(main.index)) #POST 방식이면 데이터 저장, 데이터 저장이 완료되면 main.index로 리다이렉트
+        return redirect(url_for('main.index')) #POST 방식이면 데이터 저장, 데이터 저장이 완료되면 main.index로 리다이렉트
     return render_template('question/question_form.html',form=form) # GET 방식으면 질문 등록 페이지 렌더링
