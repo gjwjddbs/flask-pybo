cae793aa830acd801149b5ef650807744deef0ce
gjwjddbs <apexgenuine@gmail.com>
2025-03-30 22:59:30 +0900

    feat: 질문 및 답변 수정/삭제 기능 추가
    - question_detail.html에 질문 수정, 삭제 버튼 추가
    - question_detail.html에 답변 수정, 삭제 버튼 추가
    - 질문 삭제 시 알림창 표시 기능 추가 (question_detail.html)
    - 질문 수정 라우터 함수 추가 (question_views.py)
    - 답변 수정 라우터 함수 추가 (answer_views.py)
    - 답변 수정 폼 파일 생성 (answer_form.html)
    - 질문 및 답변 수정일자 표시 추가 (question_detail.html)

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request, render_template, g
+from flask import Blueprint, url_for, request, render_template, g, flash
 from werkzeug.utils import redirect
 
 from ..forms import AnswerForm
@@ -31,3 +31,35 @@ def create(question_id):
         db.session.commit()
         return redirect(url_for('question.detail',question_id=question_id))
     return render_template('question/question_detail.html',question=question,form=form)
+
+#답변 수정
+@bp.route('/modify/<int:answer_id>',methods=('GET','POST'))
+@login_required #답변 수정 시 로그인 여부 확인
+def modify(answer_id):
+    answer=Answer.query.get_or_404(answer_id)
+    if g.user != answer.user:
+        flash('수정 권한이 없습니다.')# 답변 수정 권한이 없는 경우 오류 메세지 출력
+        return redirect(url_for('question.detail',question_id = answer.quetion.id))
+    if request.method == 'POST':
+        form = AnswerForm()
+        if form.validate_on_submit():
+            form.populate_obj(answer)
+            answer.modify_date = datetime.now()
+            db.session.commit()
+            return redirect(url_for('question.detail',question_id = answer.question.id))
+    else:
+        form = AnswerForm(obj=answer) #데이터베이스에서 조회한 답변 객체를 obj로 설정하여 폼에 초기값을 설정
+    return render_template('answer/answer_form.html',answer=answer,form=form) #GET 방식이면 답변 수정 페이지 렌더링
+
+#답변 삭제
+@bp.route('/delete/<int:answer_id>')
+@login_required #답변 삭제 시 로그인 여부 확인
+def delete(answer_id):
+    answer = Answer.query.get_or_404(answer_id)
+    question_id = answer.question.id
+    if g.user != answer.user:
+        flash('삭제 권한이 없습니다') #답변 삭제 권한이 없는 경우 오류 메세지 출력
+    else:
+        db.session.delete(answer)
+        db.session.commit()
+    return redirect(url_for('question.detail',question_id=question_id)) #답변 삭제 후 질문 상세 페이지로 리다이렉트
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint , render_template, request, redirect, url_for, g
+from flask import Blueprint , render_template, request, redirect, url_for, g, flash
 
 from pybo.models import Question
 
@@ -29,7 +29,7 @@ def detail(question_id):
     return render_template('question/question_detail.html',question=question,form=form)
 
 @bp.route('/create/', methods=('GET','POST'))
-@login_required #로그인 애너테이션 적용
+@login_required #로그인 애너테이션 적용, 로그인 여부 확인
 def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit(): #폼 데이터의 정합성 검사
@@ -38,3 +38,34 @@ def create():
         db.session.commit()
         return redirect(url_for('main.index')) #POST 방식이면 데이터 저장, 데이터 저장이 완료되면 main.index로 리다이렉트
     return render_template('question/question_form.html',form=form) # GET 방식으면 질문 등록 페이지 렌더링
+
+#질문 수정
+@bp.route('/modify/<int:question_id>',methods=('GET','POST'))
+@login_required #질문 수정 시 로그인 여부 확인
+def modify(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('수정 권한이 없습니다.') # 수정 권한이 없을 경우 오류 메시지 출력
+        return redirect(url_for('question.detail',question_id=question_id))
+    if request.method == 'POST': #POST 방식이면 질문 수정 처리
+        form  = QuestionForm()
+        if form.validate_on_submit(): #폼 데이터의 검증
+            form.populate_obj(question) #form의 데이터를 question 객체에 저장
+            question.modify_date = datetime.now()
+            db.session.commit()
+            return redirect(url_for('question.detail',question_id=question_id))
+    else:
+        form = QuestionForm(obj=question)# 데이터베이스에서 조회한 질문 객체를 obj로 설정하여 폼에 초기값을 설정
+    return render_template('question/question_form.html',form=form) #GET 방식이면 질문 수정 페이지 렌더링
+
+#질문 삭제
+@bp.route('/delete/<int:question_id>')
+@login_required #질문 삭제 시 로그인 여부 확인
+def delete(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('삭제 권한이 없습니다.') # 삭제 권한이 없을 경우 오류 메세지 출력
+        return redirect(url_for('quetion.detail',question_id=question_id))
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(url_for('question._list'))
