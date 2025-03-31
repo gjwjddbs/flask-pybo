a776e5f87b75b7a01fad1005db3c23ad760abd4b
gjwjddbs <apexgenuine@gmail.com>
2025-03-31 23:47:34 +0900

    feat: 댓글 모델 추가
    - models.py에 댓글 모델 추가

--- pybo/models.py
+++ pybo/models.py
@@ -1,5 +1,6 @@
 from pybo import db
 
+#질문 모델
 class Question(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     subject=db.Column(db.String(200),nullable=False)
@@ -10,6 +11,7 @@ class Question(db.Model):
     user=db.relationship('User',backref=db.backref('question_set'))
     modify_date = db.Column(db.DateTime(),nullable=True)
 
+#답변 모델
 class Answer(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     question_id=db.Column(db.Integer,db.ForeignKey('question.id',ondelete='CASCADE'))
@@ -21,9 +23,22 @@ class Answer(db.Model):
     user=db.relationship('User',backref=db.backref('answer_set'))
     modify_date = db.Column(db.DateTime(),nullable=True)
     
+#회원가입 모델
 class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(150),unique=True,nullable=False)
     password=db.Column(db.String(200),nullable=False)
     email=db.Column(db.String(120),unique=True,nullable=False)
     
+#댓글 모델
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'),nullable=False)
+    user=db.relationship('User',backref=db.backref('comment_set'))
+    content = db.Column(db.Text(),nullable=False)
+    create_date = db.Column(db.DateTime(),nullable=False)
+    modify_date = db.Column(db.DateTime())
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id',ondelete='CASCADE'),nullable=True)
+    question = db.relationship('Question',backref=db.backref('comment_set'))
+    answer_id = db.Column(db.Integer, db.ForeignKey('answer.id',ondelete='CASCADE'),nullable=True)
+    answer = db.relationship('Answer', backref = db.backref('comment_set'))
