69125e8b4b996f4777d951fb4c6766bb4a79bc31
gjwjddbs <apexgenuine@gmail.com>
2025-04-02 22:53:17 +0900

    feat: 댓글 삭제 및 수정 기능 추가
    - comment_form.html 추가하여 댓글 입력 템플릿 생성
    - comment_views.py에 댓글 수정 및 삭제 함수 추가
    
    style: CSS 코드 정렬
    
    - bootstrap.min.css 코드 정렬
    - .comment 클래스 선택자의 font-size 속성 크기 변경(style.css)

--- pybo/views/comment_views.py
+++ pybo/views/comment_views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, render_template, g, request
+from flask import Blueprint, url_for, render_template, g, request, flash 
 from werkzeug.utils import redirect
 
 from pybo import db
@@ -10,7 +10,7 @@ from pybo.views.auth_views import login_required
 
 bp = Blueprint('comment',__name__,url_prefix='/comment')
 
-#댓글 등록 함수 생성
+#댓글 등록 함수 
 @bp.route('/create/question/<int:question_id>',methods=('GET','POST'))
 @login_required
 def create_question(question_id):
@@ -22,3 +22,35 @@ def create_question(question_id):
         db.session.commit()
         return redirect(url_for('question.detail',question_id=question_id))
     return render_template('comment/comment_form.html',form=form)
+
+#질문 댓글 수정 함수
+@bp.route('/modify/question/<int:comment_id>',methods=('GET','POST'))
+@login_required
+def modify_question(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    if g.user != comment.user:
+        flash('수정권한이 없습니다.')
+        return redirect(url_for('question.detail',question_id=comment.question.id))
+    if request.method == 'POST':
+        form = CommentForm()
+        if form.validate_on_submit():
+            form.populate_obj(comment)
+            comment.modify_date = datetime.now() #수정일시 저장
+            db.session.commit()
+            return redirect(url_for('question.detail',question_id=comment.question.id))
+    else: #GET 요청일 경우 기존 댓글을 조회
+        form = CommentForm(obj=comment)
+    return render_template('comment/comment_form.html',form=form)
+
+#질문 댓글 삭제 함수 
+@bp.route('/delete/question/<int:comment_id>')
+@login_required
+def delete_question(comment_id):
+    comment=Comment.query.get_or_404(comment_id)
+    question_id = comment.question.id
+    if g.user != comment.user:
+        flash('삭제권한이 없습니다.')
+        return redirect(url_for('question.detail',question_id=question_id))
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect(url_for('question.detail',question_id=question_id))
